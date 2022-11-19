a9e339362a3eae4192a08e1204c7b40c405b0b67
LeonardoERZ <lriveraz1700@alumno.ipn.mx>
2022-11-19 13:35:05 -0600

    Práctica 5 Parte 1: Interfaces y Módulos

---
+++ labp5_use_functions.py
@@ -0,0 +1,17 @@
+#===============================================
+# Práctica 5 Parte 1 Paradigmas de Programación
+# Autor:     Rivera Zacarias Leonardo Enrique
+# Grupo:     2AV1
+# Profesor:  Dr. Julian Tercero Becerra Sagredo
+#===============================================
+
+#======================================================
+# De functions.py traer las funciones suma y promedio
+#======================================================
+from labp5_functions import suma,promedio
+print(suma(100,200))
+print(promedio(100,200))
+
+#====================================
+#       FIN DE LA PRÁCTICA
+#====================================
