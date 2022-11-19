9452f452448551e5d609789bebed923807858431
LeonardoERZ <lriveraz1700@alumno.ipn.mx>
2022-11-19 13:13:25 -0600

    Módulo Para Práctica 5 Parte 1

---
+++ labp5_functions.py
@@ -0,0 +1,21 @@
+#===============================================
+# -------- Módulo para _use_functions.py -------
+# Práctica 5 Paradigmas de Programación
+# Autor:     Rivera Zacarias Leonardo Enrique
+# Grupo:     2AV1
+# Profesor:  Dr. Julian Tercero Becerra Sagredo
+#===============================================
+def suma(x,y):
+    return x+y
+
+def promedio(x,y):
+    return 0.5*(x+y)
+
+def potencia(x,y):
+    return x**y
+
+#=====================================
+#           FIN DEL MÓDULO 
+#=====================================
+
+
